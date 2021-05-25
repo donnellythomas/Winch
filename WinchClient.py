d799f50b5434881c71d2667cb0d37dca4770841c
thomasdonnelly <donnelly.thomas21@gmail.com>
2021-05-25 08:34:00 -0400

    Added soak fields and sensor toggle buttons, toggle buttons are a bit broken.

--- WinchClient.py
+++ WinchClient.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-import tkinter as tk
+import Tkinter as tk
 from time import sleep
 
 # IP and port for UDP current values are what I was using for testing
@@ -19,8 +19,9 @@ master.title("CTD Winch Interface")
 # Command that will be sent over UDP
 current_command = ""
 
-#soak depth 1.1 meters
-#soak time 1 minute
+
+# soak depth 1.1 meters
+# soak time 1 minute
 def set_command(command):
     """
     Change the current command
@@ -48,16 +49,54 @@ def send_command():
         sleep(.3)
 
 
+def toggle(button, command, tog=[0]):
+    '''
+    a list default argument has a fixed address
+    '''
+    tog[0] = not tog[0]
+    if tog[0]:
+        set_command(command + "OFF")
+        button.config(text='off')
+    else:
+        set_command(command + "ON")
+        button.config(text='on')
+
+
 # Buttons for each command
-tk.Button(master, text='Manual Out', command=lambda *args: set_command("MANIN")).pack()
-tk.Button(master, text='Manual In', command=lambda *args: set_command("MANOUT")).pack()
-tk.Button(master, text='Stop', command=lambda *args: set_command("STOP")).pack()
+tk.Button(master, text='Manual Out', command=lambda *args: set_command("MANIN")).grid(row=0, column=0)
+tk.Button(master, text='Manual In', command=lambda *args: set_command("MANOUT")).grid(row=1, column=0)
+tk.Label(master, text="Slack Sensor:").grid(row=0, column=1)
+tk.Label(master, text="Dock Sensor:").grid(row=0, column=2)
+tk.Label(master, text="Line Sensor:").grid(row=0, column=3)
+slack_btn = tk.Button(text="on", width=12, command=lambda *args: toggle(slack_btn, "SLACK"))
+slack_btn.grid(row=1, column=1)
+dock_btn = tk.Button(text="on", width=12, command=lambda *args: toggle(dock_btn, "DOCK"))
+dock_btn.grid(row=1, column=2)
+line_btn = tk.Button(text="on", width=12, command=lambda *args: toggle(line_btn, "LINE"))
+line_btn.grid(row=1, column=3)
+
+tk.Button(master, text='Stop', command=lambda *args: set_command("STOP")).grid(row=2, column=0)
 # Label and input for
-tk.Label(master, text="Cast To").pack()
-entry = tk.Entry(master)
-entry.pack()
-tk.Button(master, text='Cast', command=lambda *args: set_command("CAST " + entry.get())).pack()
-tk.Button(master, text='Read Data', command=lambda *args: set_command("READDATA")).pack()
+tk.Label(master, text="Cast To:").grid(row=3, column=0)
+cast_depth = tk.Entry(master)
+cast_depth.grid(row=3, column=1)
+tk.Label(master, text="meters").grid(row=3, column=2)
+
+tk.Label(master, text="Soak Depth:").grid(row=4, column=0)
+soak_depth = tk.Entry(master)
+soak_depth.insert(0, "1.1")
+soak_depth.grid(row=4, column=1)
+tk.Label(master, text="meters").grid(row=4, column=2)
+
+tk.Label(master, text="Soak Time:").grid(row=5, column=0)
+soak_time = tk.Entry(master)
+soak_time.insert(0, "60")
+soak_time.grid(row=5, column=1)
+tk.Label(master, text="seconds").grid(row=5, column=2)
+
+tk.Button(master, text='Cast', command=lambda *args: set_command(
+    "CAST " + cast_depth.get() + " " + soak_depth.get() + " " + soak_time.get())).grid(row=6, column=1)
+tk.Button(master, text='Read Data', command=lambda *args: set_command("READDATA")).grid(row=7, column=0)
 t = threading.Thread(target=send_command)
 t.start()
 master.mainloop()
